0528c3279f7e9934bac021b014707c646c118702
Supawich2307 <44920680+Supawich2307@users.noreply.github.com>
2020-05-11 12:02:53 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -5,7 +5,7 @@ let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('./os.html', null, function (error, data) {
+    fs.readFile('./os_1.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
